463848b1ade50c4ff5c9402425a87903a463835b
Elina <ya.ilina4@yandex.ru>
2021-10-02 23:44:24 +0300

    Master branch for Architectures_and_patterns_Java created

---
+++ src/test/java/ru/geekbrains/architectures_and_patterns/ArchitecturesAndPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.architectures_and_patterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitecturesAndPatternsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
